32e58e82b876ebb8c1c2eddde7945e28ffd98b91
49455 <49455@LAPTOP-PMNFB7KG>
2019-05-25 15:46:59 +0800

    11

---
+++ src/main/java/org/zhegemeihunzhijiefa/weixin/JzpApplication.java
@@ -0,0 +1,13 @@
+package org.zhegemeihunzhijiefa.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JzpApplication.class, args);
+	}
+
+}
