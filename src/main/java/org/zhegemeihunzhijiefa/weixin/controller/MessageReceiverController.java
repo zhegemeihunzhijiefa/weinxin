0baacc91bf13733e9825067c7e880f8944c0f791
49455 <49455@LAPTOP-PMNFB7KG>
2019-06-22 21:40:21 +0800

    1231

--- src/main/java/org/zhegemeihunzhijiefa/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/zhegemeihunzhijiefa/weixin/controller/MessageReceiverController.java
@@ -2,6 +2,7 @@ package org.zhegemeihunzhijiefa.weixin.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //控制器：负责接受用户的请求参数，调用业务逻辑层代码，返回视图、结果给客户端（浏览器）
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageReceiverController {
   @GetMapping
   public String echo(//
-		  String singnature,
-		  String timestamp,
-		  String nonce,
-		  String echostr
+		  @RequestParam("signature")String singnature,
+		  @RequestParam("timestamp")String timestamp,
+		  @RequestParam("nonce")String nonce,
+		  @RequestParam("echostr")String echostr
 		  ){
 	  return echostr;
   }
