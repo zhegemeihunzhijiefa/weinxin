1ef81f7268c1db771020d80fa5d8362cfa2ea932
49455 <49455@LAPTOP-PMNFB7KG>
2019-06-22 22:26:11 +0800

    123

--- src/main/java/org/zhegemeihunzhijiefa/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/zhegemeihunzhijiefa/weixin/controller/MessageReceiverController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 //控制器：负责接受用户的请求参数，调用业务逻辑层代码，返回视图、结果给客户端（浏览器）
 @RestController
-@RequestMapping("/Jzp/weixin/receiver")
+@RequestMapping("/weinxin/weixin/receiver")
 public class MessageReceiverController {
   @GetMapping
   public String echo(//
