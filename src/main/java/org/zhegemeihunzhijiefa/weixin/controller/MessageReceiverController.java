f682c67ebe96de1727bd502ad7ce9ef2ef5e3b56
49455 <49455@LAPTOP-PMNFB7KG>
2019-07-01 00:39:57 +0800

    1111

--- src/main/java/org/zhegemeihunzhijiefa/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/zhegemeihunzhijiefa/weixin/controller/MessageReceiverController.java
@@ -1,14 +1,22 @@
 package org.zhegemeihunzhijiefa.weixin.controller;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 //控制器：负责接受用户的请求参数，调用业务逻辑层代码，返回视图、结果给客户端（浏览器）
 @RestController
 @RequestMapping("/weinxin/weixin/receiver")
 public class MessageReceiverController {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MessageReceiverController.class);
   @GetMapping
   public String echo(//
 		  @RequestParam("signature")String singnature,
@@ -18,4 +26,18 @@ public class MessageReceiverController {
 		  ){
 	  return echostr;
   }
+  
+  
+  @PostMapping
+  //@RequestBody注解表示把请求内容获取出来，并且转换为String传入给xml参数。
+  public String onMessage(@RequestBody String xml) {
+	  // 收到消息
+	  LOG.trace("收到的消息原文：\n{}\n--------------------------",xml);
+	  // 转换消息
+	  // 把消息丢入队列
+	  // 消费队列中的消息
+	  // 产生客服消息
+	  return "success";
+  }
+  
 }
